5177e4c079b60ee4fe3e77f774c8f77ff53b36f8
Jason Jackson <jwjackson.wsu@gmail.com>
2022-01-09 23:30:19 -0800

    All content filled. Needs refactoring

--- src/index.js
+++ src/index.js
@@ -103,11 +103,22 @@ const displayContactPage = function() {
     const contactH1 = document.createElement('h1');
     contactH1.textContent = 'Contact Us';
     
-    const infoPara = document.createElement('p');
-    infoPara.textContent = 'You may contact us here!';
+    const numberPara = document.createElement('p');
+    numberPara.classList.add('contactInfo');
+    numberPara.textContent = '📞 123 456 789';
+
+    const locationPara = document.createElement('p');
+    locationPara.classList.add('contactInfo');
+    locationPara.textContent = '🏠 1234 W. Riverside Ave, Ourtown, WA 99999';
+
+    const emailPara = document.createElement('p');
+    emailPara.classList.add('contactInfo');
+    emailPara.textContent = ' ✉ jwjackburgershack@realemail.com';
 
     contentDiv.appendChild(contactH1);
-    contentDiv.appendChild(infoPara);
+    contentDiv.appendChild(numberPara);
+    contentDiv.appendChild(locationPara);
+    contentDiv.appendChild(emailPara);
 
 };
 
